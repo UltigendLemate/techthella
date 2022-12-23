082d148b870458a81dfacf64da49f831eb0d92de
anupammaurya6767 <anupammaurya6767@gmail.com>
2022-12-24 05:03:59 +0530

    chatbot

---
+++ components/bot.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import Chatbot from 'react-best-chatbot';
+
+ const theme = {
+  primaryColor: "#FFFFFF",
+  secondaryColor: "rgb(34, 197, 94)",
+  textPrimaryColor: "rgb(34, 197, 94)",
+  textSecondaryColor: "#FFFFFF",
+  containerStyle: {
+    width: 310,
+    boxShadow: "none"
+  }
+};
+  
+const steps = [
+   {
+    id: 1,
+    content: "Hi!",
+    goTo: 2,
+  },
+  {
+    id: 2,
+    content: "What can I help you with?",
+    options: [
+      {
+        content: "Login Issue",
+        value: 1,
+        goTo: "login",
+      },
+      {
+        content: "General Issue",
+        value: 2,
+        goTo: "general",
+      },
+      {
+        content: "Payment related issue",
+        value: 3,
+        goTo: "payment",
+      },
+    ],
+  },
+  {
+    id: "login",
+    content: "A Technician assigned to you",
+    goTo: 2,
+  },
+  {
+    id: "general",
+    content: "Kindly Describe your issue",
+    receiveInput: true,
+    goto:5,
+  },
+  {
+    id: 5,
+    content: "A Technician assigned to you",
+  },
+  {
+    id: "payment",
+    content: "A Technician assigned to you",
+    end: true,
+  },
+];
+
+
+const options = {
+  header: "Techthella Bot",
+  endContent: "See ya 👋",
+  
+};
+
+const CustomChatbot = () => (
+  <Chatbot steps={steps} options={options}  />
+);
+
+
+
+export default CustomChatbot;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Footer from '../components/Footer'
+import CustomChatbot from '../components/bot.js' 
 
 export default function App({ Component, pageProps }: AppProps) {
   return(<>
    <Component {...pageProps} />
+   <CustomChatbot />
   <Footer/>
   </>)
 }
