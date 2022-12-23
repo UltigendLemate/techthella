9c23bba6128f160b78dba15aa6d6d97522bc9adc
theishanbh <theishanbh@gmail.com>
2022-12-23 21:14:47 +0530

    bugs fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   // make menu hover toggle
 
   const [menuPricing, setMenuPricing] = useState(false);
-  const [menuServices, setMenuServices] = useState(true);
+  const [menuServices, setMenuServices] = useState(false);
 
   return (
     <>
@@ -13,15 +13,13 @@ export default function Home() {
         <nav className="relative border-gray-600 h-{15vh} bg-gray-900 ">
           <div className="flex flex-wrap h-[15vh]  justify-between items-center self-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
             <a href="" className="flex items-center">
-              <span className="self-center text-4xl font-semibold whitespace-nowrap dark:text-white">
+              <span className="self-center text-4xl font-semibold whitespace-nowrap text-white">
                 <span className="text-green-500">tech</span>THELLA
               </span>
             </a>
             <button
               id="mega-menu-full-cta-button"
-             
-              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-       
+              className="inline-flex items-center p-2 ml-1 text-sm rounded-lg md:hidden focus:outline-none focus:ring-2  text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -46,16 +44,16 @@ export default function Home() {
                 <li className="flex  items-center">
                   <a
                     href="#"
-                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700 text-xl"
+                    className="block py-2 pr-4 pl-3 border-b   md:border-0 md:p-0 text-gray-400 md:hover:text-blue-500 hover:bg-gray-700 hover:text-blue-500 md:hover:bg-transparent border-gray-700 text-xl"
                     aria-current="page"
                   >
                     Home
                   </a>
                 </li>
-                <li className="flex items-center" >
+                <li className="flex items-center">
                   <a
                     href="#"
-                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700 text-xl"
+                    className="block py-2 pr-4 pl-3 border-b   md:border-0 md:p-0 text-gray-400 md:hover:text-blue-500 hover:bg-gray-700 hover:text-blue-500 md:hover:bg-transparent border-gray-700 text-xl"
                     aria-current="page"
                   >
                     About
@@ -64,12 +62,12 @@ export default function Home() {
                 <li className="flex">
                   <button
                     id="mega-menu-full-cta-dropdown-button"
-         
                     data-dropdown-placement="bottom"
-                    className="flex justify-between items-center py-2 pr-4 pl-3 w-full font-medium text-gray-700 border-b border-gray-100 md:w-auto hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700 text-xl"
+                    className="flex justify-between items-center py-2 pr-4 pl-3 w-full font-medium border-b md:w-auto md:border-0 md:p-0 text-gray-400 md:hover:text-blue-500 hover:bg-gray-700 hover:text-blue-500 md:hover:bg-transparent border-gray-700 text-xl"
                     onMouseEnter={() => {
-                      if (!menuServices) {
-                        setMenuPricing(true);
+                      setMenuPricing(true);
+                      if (menuServices) {
+                        setMenuServices(false);
                       }
                     }}
                   >
@@ -93,10 +91,11 @@ export default function Home() {
                     id="mega-menu-full-cta-dropdown-button"
                     data-collapse-toggle="mega-menu-full-cta-dropdown"
                     data-dropdown-placement="bottom"
-                    className="flex justify-between items-center py-2 pr-4 pl-3 w-full font-medium text-gray-700 border-b border-gray-100 md:w-auto hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700 text-xl"
+                    className="flex justify-between items-center py-2 pr-4 pl-3 w-full font-medium  border-b  md:w-auto md:border-0  md:p-0 text-gray-400 md:hover:text-blue-500 hover:bg-gray-700 hover:text-blue-500 md:hover:bg-transparent border-gray-700 text-xl"
                     onMouseEnter={() => {
-                      if (!menuPricing) {
-                        setMenuServices(true);
+                      setMenuServices(true);
+                      if (menuPricing) {
+                        setMenuPricing(false);
                       }
                     }}
                   >
@@ -118,7 +117,7 @@ export default function Home() {
                 <li>
                   <a
                     href="#"
-                    className="block  text-black  bg-white md:border-0 px-3 py-2 rounded-xl  md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent text-xl dark:border-gray-700"
+                    className="block  text-black  bg-white md:border-0 px-3 py-2 rounded-xl  md:hover:text-blue-500 hover:bg-gray-700 hover:text-blue-500 md:hover:bg-transparent text-xl border-gray-700"
                   >
                     Start Now
                   </a>
@@ -131,41 +130,32 @@ export default function Home() {
             id="mega-menu-full-cta-dropdown"
             className={
               menuPricing
-                ? "mt-1 bg-white border-gray-200 shadow-sm border-y dark:bg-gray-800 absolute top-20 w-full dark:border-gray-600"
+                ? "mt-1 shadow-sm border-y bg-gray-800 absolute top-20 w-full border-gray-600"
                 : "hidden"
             }
             onMouseLeave={() => setMenuPricing(false)}
           >
-            <div className="grid py-5 px-4 mx-auto max-w-screen-xl text-gray-900 dark:text-white sm:grid-cols-2 md:grid-cols-3 md:px-6">
+            <div className="grid py-5 px-4 mx-auto max-w-screen-xl  text-white sm:grid-cols-2 md:grid-cols-3 md:px-6">
               <div>
-                <a
-                  href="#"
-                  className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
-                >
+                <a href="#" className="block p-3 rounded-lg  hover:bg-gray-700">
                   <div className="font-semibold">Vendor - Pro Version</div>
-                  <span className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  <span className="text-sm font-light  text-gray-400">
                     Connect with third-party tools that youre already using.
                   </span>
                 </a>
               </div>
               <div>
-                <a
-                  href="#"
-                  className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
-                >
+                <a href="#" className="block p-3 rounded-lg  hover:bg-gray-700">
                   <div className="font-semibold">Customer - Base Version</div>
-                  <span className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  <span className="text-sm font-light  text-gray-400">
                     Connect with third-party tools that youre already using.
                   </span>
                 </a>
               </div>
               <div>
-                <a
-                  href="#"
-                  className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
-                >
+                <a href="#" className="block p-3 rounded-lg  hover:bg-gray-700">
                   <div className="font-semibold">Customer - Pro Version</div>
-                  <span className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  <span className="text-sm font-light  text-gray-400">
                     Connect with third-party tools that youre already using.
                   </span>
                 </a>
@@ -177,30 +167,24 @@ export default function Home() {
             id="mega-menu-full-cta-dropdown"
             className={
               menuServices
-                ? "mt-1 bg-white items-center border-gray-200 shadow-sm border-y absolute top-20 w-full dark:bg-gray-800 dark:border-gray-600"
+                ? "mt-1 items-center  shadow-sm border-y absolute top-20 w-full bg-gray-800 border-gray-600"
                 : "hidden"
             }
             onMouseLeave={() => setMenuServices(false)}
           >
-            <div className="flex items-center py-5 px-4 mx-auto max-w-screen-xl text-gray-900 dark:text-white sm:grid-cols-2 md:grid-cols-3 md:px-6">
+            <div className="flex items-center py-5 px-4 mx-auto max-w-screen-xl text-white sm:grid-cols-2 md:grid-cols-3 md:px-6">
               <div>
-                <a
-                  href="#"
-                  className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
-                >
+                <a href="#" className="block p-3 rounded-lg  hover:bg-gray-700">
                   <div className="font-semibold">Segmentation</div>
-                  <span className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  <span className="text-sm font-light  text-gray-400">
                     Connect with third-party tools that youre already using.
                   </span>
                 </a>
               </div>
               <div>
-                <a
-                  href="#"
-                  className="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700"
-                >
+                <a href="#" className="block p-3 rounded-lg  hover:bg-gray-700">
                   <div className="font-semibold">Marketing CRM</div>
-                  <span className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  <span className="text-sm font-light  text-gray-400">
                     Connect with third-party tools that youre already using.
                   </span>
                 </a>
