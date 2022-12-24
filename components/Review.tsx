632f311cfbbf216a0fcdb367d4a6f76f0adc9698
Chaitanya <chaitanya.anand@gmail.com>
2022-12-24 06:00:10 +0530

    push

--- components/Review.tsx
+++ components/Review.tsx
@@ -1,22 +1,17 @@
-import React from 'react'
-import { useState } from 'react';
+import React,{useState} from 'react'
 
-const Review = () => {
-  
+
+type Props={}
+const Review = (props:Props) => {
+
   const [email, setemail] = useState("");
   const [number, setnumber] = useState("");
   const [msg, setmsg] = useState("");
   const value = false;
-  const changekro2 = (e) => {
-    setemail(e.target.value);
-  };
-  const changekro4 = (e) => {
-    setnumber(e.target.value);
-  };
-  const changekro3 = (e) => {
-    setmsg(e.target.value);
-  };
-  const handleSubmit = async (e) => {
+
+
+
+  async function handleSubmit (e)  {
     e.preventDefault();
 
     const data = [
@@ -62,27 +57,32 @@ const Review = () => {
 
     const response = await fetch(endpoint, options);
     const response2 = await fetch(endpoint2, options2);
+  };
   return (
-    <>
-    <div className="text-[#fc6441] font-bold text-4xl mt-20">
-          Hey there, What's your review for Ravi Kumar ?
+    <div className=''>
+    <div className="text-[#fc6441] fflex flex-col justify-center items-centeront-bold text-4xl mt-20">
+          Hey there, Whats your review for Ravi Kumar ?
         </div>
       <form className="border-2 border-[#fc6441] shadow-lg p-2 flex flex-col w-[600px]">
         
-        <input className='text-xl font-semibold pb-3 p-4' placeholder="Vendor Number" value={number} onChange={changekro4}/>
-        <input className='text-xl font-semibold pb-3 p-4' placeholder="Your Review" value={msg} onChange={changekro3}/>
-        <input className='text-xl font-semibold pb-3 p-4' placeholder="Your Review" value={email} onChange={changekro2}/>
-        <button onClick={handleSubmit} 
+        <input className='text-xl font-semibold pb-3 p-4 border-2 border-black' placeholder="Vendor Number" value={number} onChange={(e) => {
+    setnumber(e.target.value);
+  }}/>
+        <input className='text-xl font-semibold pb-3 p-4' placeholder="Your Review" value={msg} onChange={(e) => {
+    setmsg(e.target.value);
+  }}/>
+        <input className='text-xl font-semibold pb-3 p-4' placeholder="Your Review" value={email} onChange={(e) => {
+    setemail(e.target.value);
+  }}/>
+        <button onClick={(e) => handleSubmit(e)} 
       className="bg-[#fc6441] w-2/5 rounded-xl p-2 text-white font-semibold">
         Submit
       </button>
 
         
       </form>
-      </>
-
+      </div>
   )
 }
-}
 
-export default Review;
+export default Review
